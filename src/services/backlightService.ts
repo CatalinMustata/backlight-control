d595b3d32dd5d8fdb9842f72e5b3217d358b3a65
Catalin Mustata <catalin.mustata@gmail.com>
2021-05-29 20:39:28 +0300

    Added transition mechanism to backlight changes

--- src/services/backlightService.ts
+++ src/services/backlightService.ts
@@ -8,6 +8,15 @@ export default class BacklightService {
 
     private backlightCtrl: Gpio
 
+    private currentValue: number
+
+    private targetValue: number
+
+    private transitionAnimator: NodeJS.Timeout | null
+
+    // duration of any
+    private readonly TRANSITION_DURATION_MS = 4000
+
     constructor() {
         if (process.env.LOCAL_MODE) return
 
@@ -25,12 +34,38 @@ export default class BacklightService {
     public setBacklight(value?: number) {
         // if no value, display should be off
         if (value === null) {
+            this.currentValue = 0
+            this.targetValue = 0
             this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, 0)
             return
         }
 
         // clamp values to 0 - 100%
         value = Math.min(Math.max(0, value), 100)
+
+        this.targetValue = value
+
+        if (this.transitionAnimator) {
+            clearInterval(this.transitionAnimator)
+        }
+
+        // transition in 1% steps
+        const transitionSteps = Math.abs(this.targetValue - this.currentValue)
+        const step = (this.currentValue < this.targetValue) ? 1 : -1
+
+        this.transitionAnimator = setInterval(() => {
+            // transition until we've reached the target value
+            if (this.currentValue != this.targetValue) {
+                this.setBacklightActual(this.currentValue + step)
+            } else {
+                clearInterval(this.transitionAnimator)
+                this.transitionAnimator = null
+            }
+        }, this.TRANSITION_DURATION_MS / transitionSteps)
+    }
+
+    private setBacklightActual(value: number) {
+        this.currentValue = value
 
         const dutyCycle = Math.floor(this.MIN_LEVEL + (value * this.AMBITUS) / 100)
 
