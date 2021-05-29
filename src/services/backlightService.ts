6dc461a5bac990d2c5a35d461e38d8eeeb7d3905
Catalin Mustata <catalin.mustata@gmail.com>
2021-05-29 22:27:51 +0300

    Reworked transition algorithm to handle display on/off

--- src/services/backlightService.ts
+++ src/services/backlightService.ts
@@ -8,9 +8,9 @@ export default class BacklightService {
 
     private backlightCtrl: Gpio
 
-    private currentValue: number = 50
+    private currentValue: number = 0
 
-    private targetValue: number
+    private targetValue: number = 50
 
     private transitionAnimator: NodeJS.Timeout | null
 
@@ -18,12 +18,13 @@ export default class BacklightService {
     private readonly TRANSITION_DURATION_MS = 4000
 
     constructor() {
+        // if running locally on a PC, there's no GPIO to play with
         if (process.env.LOCAL_MODE) return
 
         console.log("Initializing backlight control")
         this.backlightCtrl = new Gpio(19, { mode: Gpio.OUTPUT })
 
-        this.setBacklightActual(this.currentValue)
+        this.transitionBacklight() // init
     }
 
     /**
@@ -36,10 +37,9 @@ export default class BacklightService {
     public setBacklight(value?: number) {
         // if no value, display should be off
         if (value === null) {
-            this.currentValue = 0
-            this.targetValue = 0
+            this.targetValue = null
             console.log(`Current is: ${this.currentValue}, target is ${this.targetValue}`)
-            this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, 0)
+            this.transitionBacklight
             return
         }
 
@@ -48,6 +48,21 @@ export default class BacklightService {
 
         this.targetValue = value
 
+        this.transitionBacklight()
+    }
+
+    private transitionBacklight() {
+        if (this.targetValue === null) { // turn display off
+            this.currentValue = null
+            this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, 0)
+            return
+        } else if (this.currentValue === null) { // turn display on to target value
+            this.currentValue = this.targetValue
+            const targetDutyCycle = this.dutyCycleFor(this.targetValue)
+            this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, targetDutyCycle)
+        }
+
+        // stop current transition
         if (this.transitionAnimator) {
             clearInterval(this.transitionAnimator)
         }
@@ -60,7 +75,9 @@ export default class BacklightService {
         this.transitionAnimator = setInterval(() => {
             // transition until we've reached the target value
             if (this.currentValue != this.targetValue) {
-                this.setBacklightActual(this.currentValue + step)
+                const dutyCycle = this.dutyCycleFor(this.currentValue + step)
+                console.log(`Will set display PWM to ${dutyCycle}`)
+                this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, dutyCycle)
             } else {
                 clearInterval(this.transitionAnimator)
                 this.transitionAnimator = null
@@ -68,12 +85,9 @@ export default class BacklightService {
         }, this.TRANSITION_DURATION_MS / transitionSteps)
     }
 
-    private setBacklightActual(value: number) {
-        this.currentValue = value
+    private dutyCycleFor(value: number | null) {
+        if (value === null) return 0
 
-        const dutyCycle = Math.floor(this.MIN_LEVEL + (value * this.AMBITUS) / 100)
-
-        console.log(`Will set display PWM to ${dutyCycle}`)
-        this.backlightCtrl.hardwarePwmWrite(this.HW_PWM_FREQ, dutyCycle)
+        return Math.floor(this.MIN_LEVEL + (value * this.AMBITUS) / 100)
     }
 }
